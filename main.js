f244c00f0abf389fc7ea1a12879134329bcfac36
Ju Zuu Hlaing <juzuuhlaing_ju@cmu.ac.th>
2025-06-27 10:16:17 +0700

    8.5 onSale text is added to string

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-const { createApp, ref } = Vue;
+const { createApp, ref,computed } = Vue;
 
 createApp({
   setup() {
     const product = ref('Boots');
-    const image = ref('./assets/images/socks_green.jpg');
+    const brand=ref('SE 331')
     const link = ref('https://www.camt.cmu.ac.th/');
-    const inStock = ref(false); // keep this boolean
     const inventory = ref(100);
     const details = ref(['50% cotton', '20% wool', '30% polyester']);
     const variants = ref([
-      { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-      { id: 2235, color: 'blue',  image: './assets/images/socks_blue.jpg' }
+      { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity:50, onSale: true },
+      { id: 2235, color: 'blue',  image: './assets/images/socks_blue.jpg',quantity:0 ,onSale:false}
     ]);
+    const selectedVariant=ref(0)
     const sizes = ref(['S', 'M', 'L']);
     const cart = ref(0);
 
     function addToCart() {
       cart.value += 1;
     }
+
+  const title = computed(() => {
+  const current = variants.value[selectedVariant.value];
+  return current.onSale
+    ? `${brand.value} ${product.value} — On Sale! 🛍️`
+    : `${brand.value} ${product.value}`;
+});
+
 
     function updateImage(variantImage) {
       image.value = variantImage;
@@ -27,8 +35,22 @@ createApp({
       inStock.value = !inStock.value; // flip the boolean :contentReference[oaicite:1]{index=1}
     }
 
+    function updateVariant(index){
+      selectedVariant.value=index;
+    }
+
+    const image=computed(()=>{
+      return variants.value[selectedVariant.value].image
+    })
+
+     const inStock=computed(()=>{
+      return variants.value[selectedVariant.value].quantity>0
+    })
+
+   
+
     return {
-      product,
+      title,
       image,
       link,
       inStock,
@@ -40,6 +62,7 @@ createApp({
       addToCart,
       updateImage,
       toggleInStock,
+      updateVariant
     };
   }
 }).mount('#app');
